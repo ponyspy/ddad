ea1a98bd2845e3e28bf10dba404ad76f4cd856aa
ponyspy <desade4me@gmail.com>
2018-08-08 14:19:54 +0300

    cv

--- apps/ddad/routes/admin/cv.js
+++ apps/ddad/routes/admin/cv.js
@@ -27,22 +27,22 @@ exports.edit_form = function(req, res) {
 		desc_ru: function(callback) {
 			if (!post.desc.ru) return callback(null);
 
-			fs.writeFile(__app_root + '/static/desc_ru.html', 'utf8', post.desc.ru, callback);
+			fs.writeFile(__app_root + '/static/desc_ru.html', post.desc.ru, callback);
 		},
 		desc_en: function(callback) {
 			if (!post.desc.en) return callback(null);
 
-			fs.writeFile(__app_root + '/static/desc_en.html', 'utf8', post.desc.en, callback);
+			fs.writeFile(__app_root + '/static/desc_en.html', post.desc.en, callback);
 		},
 		adress_ru: function(callback) {
 			if (!post.adress.ru) return callback(null);
 
-			fs.writeFile(__app_root + '/static/adress_ru.html', 'utf8', post.adress.ru, callback);
+			fs.writeFile(__app_root + '/static/adress_ru.html', post.adress.ru, callback);
 		},
 		adress_en: function(callback) {
 			if (!post.adress.en) return callback(null);
 
-			fs.writeFile(__app_root + '/static/adress_en.html', 'utf8', post.adress.en, callback);
+			fs.writeFile(__app_root + '/static/adress_en.html', post.adress.en, callback);
 		}
 	}, function(err, results) {
 		res.redirect('back');
