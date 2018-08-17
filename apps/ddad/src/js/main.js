295379ad6463ee1439651a9c0884d0aa2d3411de
ponyspy <desade4me@gmail.com>
2018-08-17 19:20:34 +0300

    main

--- apps/ddad/src/js/main.js
+++ apps/ddad/src/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
+	$('.d_item').on('mouseenter', function(e) {
+		var id = $(this).attr('class').split(' ')[1];
 
+		$('.p_item, .d_item').not('.' + id).addClass('hover');
+	});
+
+	$('.d_item').on('mouseleave', function(e) {
+		$('.p_item, .d_item').removeClass('hover');
+	});
 });
