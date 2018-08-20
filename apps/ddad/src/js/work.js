abb7603bccb3375e1c7adefb526b20cd4b86c411
ponyspy <desade4me@gmail.com>
2018-08-20 23:48:45 +0300

    init

--- apps/ddad/src/js/work.js
+++ apps/ddad/src/js/work.js
@@ -1,3 +1,16 @@
 $(function() {
 
+	$('.image_gallery').each(function() {
+		var $this = $(this);
+
+		var gallery = new Swiper($this.children('.swiper-container'), {
+			keyboardControl: true,
+			nextButton: $this.find('.swiper-button-next'),
+			prevButton: $this.find('.swiper-button-prev'),
+			spaceBetween: 40,
+			autoHeight: true,
+			slidesPerView: 1
+		});
+	});
+
 });
