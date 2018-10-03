3b96714bdeaa851044c2677954a256f37b2cf0da
ponyspy <desade4me@gmail.com>
2018-10-03 16:38:32 +0300

    hover

--- apps/ddad/src/js/main.js
+++ apps/ddad/src/js/main.js
@@ -6,30 +6,26 @@ $(function() {
 	});
 
 	$('.d_item')
-		.on('mouseenter', function(e) {
-			var id = $(this).attr('class').split(' ')[1];
-
-			$('.p_item, .d_item').not('.' + id).addClass('hover');
-		})
-		.on('mouseleave', function(e) {
-			$('.p_item, .d_item').removeClass('hover');
-		})
 		.on('click', function(e) {
 			if ($(this).hasClass('select')) {
-				$('.p_item, .d_item').removeClass('active select');
+				$('.p_item, .d_item').removeClass('hover select');
 				return false;
 			}
 
-			$(this).addClass('select');
+			var id = $(this).attr('class').split(' ')[1];
 
-			$('.p_item, .d_item').filter('.hover').addClass('active');
+			$('.d_item').removeClass('select').filter(this).addClass('select');
+
+			$('.p_item, .d_item').removeClass('hover').not('.' + id).addClass('hover');
 		});
 
-	$('.p_item')
+	$('.p_item').not('.hover')
 		.on('mouseenter', function(e) {
+			if ($(this).hasClass('hover')) return false;
+
 			var poster = $(this).attr('data-poster');
 
-			if (poster) $('.poster').css('background-image', 'url(' + poster + ')')
+			if (poster) $('.poster').css('background-image', 'url(' + poster + ')');
 		})
 		.on('mouseleave', function(e) {
 			$('.poster').removeAttr('style');
