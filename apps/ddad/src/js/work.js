57b02577f05055a3d870060954a071a0a2d5d056
ponyspy <desade4me@gmail.com>
2018-10-03 22:18:08 +0300

    preview

--- apps/ddad/src/js/work.js
+++ apps/ddad/src/js/work.js
@@ -13,4 +13,26 @@ $(function() {
 		});
 	});
 
+	$('.image')
+	.on('mouseenter', function(e) {
+		var path = $(this).find('img').attr('src');
+
+		$('.preview').addClass('show');
+		$('<img/>', { src: path }).appendTo('.preview');
+	})
+	.on('mouseleave', function(e) {
+		$('.preview').removeClass('show').empty();
+	});
+
+	$(document).on('mousemove', '.image', function(e) {
+		var $this = $(this);
+		var $preview = $('.preview');
+
+		var percentY = (e.pageY - $this.offset().top) / $this.height() * 1.1 - 0.30;
+		var percentX = (e.pageX - $this.offset().left) / $this.width() * 1.1 - 0.30;
+
+		$preview.scrollTop($preview.children('img').height() * percentY);
+		$preview.scrollLeft($preview.children('img').width() * percentX);
+	});
+
 });
