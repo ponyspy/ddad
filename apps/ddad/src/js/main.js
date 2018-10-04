e40fcda8803f06e022e1898149001664d561a39a
ponyspy <desade4me@gmail.com>
2018-10-04 10:45:15 +0300

    fix

--- apps/ddad/src/js/main.js
+++ apps/ddad/src/js/main.js
@@ -12,39 +12,47 @@ $(function() {
 		});
 
 	$('.p_item').each(function() {
-		var poster = $(this).attr('data-poster');
+		var $this = $(this);
+		var poster = $this.attr('data-poster');
 
 		if (poster) (new Image()).src = poster;
 
-		if (localStorage.getItem($(this).attr('data-id'))) {
-			$(this).addClass('back');
+		if (localStorage.getItem($this.attr('data-id'))) {
+			$this.addClass('back');
 		}
 	});
 
 	$('.d_item')
 		.on('click', function(e) {
-			if ($(this).hasClass('select')) {
+			var $this = $(this);
+
+			if ($this.hasClass('select')) {
 				$('.p_item, .d_item').removeClass('hover select');
 				return false;
 			}
 
-			var id = $(this).attr('class').split(' ')[1];
+			var id = $this.attr('class').split(' ')[1];
 
 			$('.d_item').removeClass('select').filter(this).addClass('select');
 
 			$('.p_item, .d_item').removeClass('hover').not('.' + id).addClass('hover');
 		});
 
-	$('.p_item').not('.hover')
+	$('.p_item')
 		.on('click', function(e) {
-			var id = $(this).attr('data-id');
+			var $this = $(this);
+			var id = $this.attr('data-id');
+
+			$this.addClass('back');
 
 			localStorage.setItem(id, 'w_id');
 		})
 		.on('mouseenter', function(e) {
-			if ($(this).hasClass('hover')) return false;
+			var $this = $(this);
 
-			var poster = $(this).attr('data-poster');
+			if ($this.hasClass('hover')) return false;
+
+			var poster = $this.attr('data-poster');
 
 			if (poster) $('.poster').css('background-image', 'url(' + poster + ')');
 		})
